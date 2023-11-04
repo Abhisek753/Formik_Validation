bb4ea56012463a6f2c0b0d72591214521de38d88
Abhisek753 <properachievement@gmail.com>
2023-11-04 20:46:34 +0530

    formik form validation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Registration from './components/Registration';
 
 function App() {
   return (
     <div className="App">
-   hii 
+     <Registration/>
     </div>
   );
 }
---
+++ src/components/Registration.jsx
@@ -0,0 +1,325 @@
+import React from "react";
+import styled from "styled-components";
+import { GlobalStyle } from "../Styles/globalStyles";
+import { useFormik } from "formik";
+import { signUpSchema } from "../schemas/index";
+
+const initialValues = {
+  name: "",
+  email: "",
+  password: "",
+  confirm_password: "",
+  age:"",
+};
+
+const Registration = () => {
+  const { values, errors, touched, handleBlur, handleChange, handleSubmit } =
+    useFormik({
+      initialValues,
+      validationSchema: formUpSchema,
+      onSubmit: (values, action) => {
+        console.log(
+          "🚀 ~ file: Registration.jsx ~ line 11 ~ Registration ~ values",
+          values
+        );
+        action.resetForm();
+      },
+    });
+  console.log(
+    "🚀 ~ file: Registration.jsx ~ line 25 ~ Registration ~ errors",
+    errors
+  );
+
+  return (
+    <>
+      <GlobalStyle />
+      <Wrapper>
+        <div className="container-parent">
+          <div className="modal">
+            <div className="modal-container">
+              <div className="modal-left">
+                <h1 className="modal-title">Welcome!</h1>
+                <p className="modal-desc">
+                  Welcome to Abhisek753 Duniya
+                </p>
+                <form onSubmit={handleSubmit}>
+                  <div className="input-block">
+                    <label htmlFor="name" className="input-label">
+                      Name
+                    </label>
+                    <input
+                      type="name"
+                      autoComplete="off"
+                      name="name"
+                      id="name"
+                      placeholder="Name"
+                      value={values.name}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                    />
+                    {errors.name && touched.name ? (
+                      <p className="form-error">{errors.name}</p>
+                    ) : null}
+                  </div>
+                  <div className="input-block">
+                    <label htmlFor="email" className="input-label">
+                      Email
+                    </label>
+                    <input
+                      type="email"
+                      autoComplete="off"
+                      name="email"
+                      id="email"
+                      placeholder="Email"
+                      value={values.email}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                    />
+                    {errors.email && touched.email ? (
+                      <p className="form-error">{errors.email}</p>
+                    ) : null}
+                  </div>
+                  <div className="input-block">
+                    <label htmlFor="password" className="input-label">
+                      Password
+                    </label>
+                    <input
+                      type="password"
+                      autoComplete="off"
+                      name="password"
+                      id="password"
+                      placeholder="Password"
+                      value={values.password}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                    />
+                    {errors.password && touched.password ? (
+                      <p className="form-error">{errors.password}</p>
+                    ) : null}
+                  </div>
+                  <div className="input-block">
+                    <label htmlFor="confirm_password" className="input-label">
+                      Confirm Password
+                    </label>
+                    <input
+                      type="password"
+                      autoComplete="off"
+                      name="confirm_password"
+                      id="confirm_password"
+                      placeholder="Confirm Password"
+                      value={values.confirm_password}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                    />
+                    {errors.confirm_password && touched.confirm_password ? (
+                      <p className="form-error">{errors.confirm_password}</p>
+                    ) : null}
+                  </div>
+                  <div className="input-block">
+                    <label htmlFor="age" className="input-label">
+                      Age
+                    </label>
+                    <input
+                      type="Number"
+                      autoComplete="off"
+                      name="age"
+                      id="age"
+                      placeholder="Age"
+                      value={values.age}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                    />
+                    {errors.password && touched.password ? (
+                      <p className="form-error">{errors.password}</p>
+                    ) : null}
+                  </div>
+                  <div className="modal-buttons">
+                    <a href="#" className="">
+                      Want to register using Gmail?
+                    </a>
+                    <button className="input-button" type="submit">
+                      Registration
+                    </button>
+                  </div>
+                </form>
+                <p className="sign-up">
+                  Already have an account? <a href="#">Sign In now</a>
+                </p>
+              </div>
+              <div className="modal-right">
+                <img
+                  src="https://cdn.dribbble.com/users/1429256/screenshots/6677821/jotform-mobile-forms.gif"
+                  alt=""
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </Wrapper>
+    </>
+  );
+};
+
+const Wrapper = styled.section`
+  .container-parent {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: #ddd;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .modal {
+    width: 100%;
+    /* height: 60px; */
+    background: rgba(51, 51, 51, 0.5);
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    transition: 0.4s;
+  }
+  .modal-container {
+    display: flex;
+    max-width: 60vw;
+    width: 100%;
+    border-radius: 10px;
+    overflow: hidden;
+    position: absolute;
+
+    transition-duration: 0.3s;
+    background: white;
+  }
+  .modal-title {
+    margin: 0;
+    font-weight: 400;
+    color: #1b75d0;
+  }
+  .form-error {
+    font-size: 1.4rem;
+    color: #b22b27;
+  }
+  .modal-desc {
+    margin: 6px 0 30px 0;
+  }
+  .modal-left {
+    padding: 60px 30px 20px;
+    background: #fff;
+    flex: 1.5;
+    transition-duration: 0.5s;
+    opacity: 1;
+  }
+
+  .modal-right {
+    flex: 2;
+    font-size: 0;
+    transition: 0.3s;
+    overflow: hidden;
+  }
+  .modal-right img {
+    width: 100%;
+    height: 100%;
+   transition-duration: 1.2s;
+  }
+
+  .modal.is-open .modal-left {
+    transform: translateY(0);
+    opacity: 1;
+    transition-delay: 0.1s;
+  }
+  .modal-buttons {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  .modal-buttons a {
+    color: rgba(51, 51, 51, 0.6);
+    font-size: 14px;
+  }
+
+  .sign-up {
+    margin: 60px 0 0;
+    font-size: 14px;
+    text-align: center;
+  }
+  .sign-up a {
+    color: red;
+  }
+
+  .input-button {
+    padding: 2rem;
+    outline: none;
+    text-transform: uppercase;
+    border: 0;
+    color:white;
+    border-radius: 4px;
+    background:#1b75d0;
+    transition: 0.3s;
+    cursor: pointer;
+    font-family: "Nunito", sans-serif;
+  }
+  .input-button:hover {
+    background: #55311c;
+  }
+
+  .input-label {
+    font-size: 11px;
+    text-transform: uppercase;
+    font-weight: 600;
+    letter-spacing: 0.7px;
+    color: #1b75d0;
+    transition: 0.3s;
+  }
+
+  .input-block {
+    display: flex;
+    flex-direction: column;
+    padding: 10px 10px 8px;
+    border: 1px solid #ddd;
+    border-radius: 4px;
+    margin-bottom: 20px;
+    transition: 0.3s;
+  }
+  .input-block input {
+    outline: 0;
+    border: 0;
+    padding: 4px 0 0;
+    font-size: 14px;
+  }
+
+  .input-block input::-moz-placeholder {
+    color: #ccc;
+    opacity: 1;
+  }
+  .input-block input:-ms-input-placeholder {
+    color: #ccc;
+    opacity: 1;
+  }
+  .input-block input::placeholder {
+    color: #ccc;
+    opacity: 1;
+  }
+  .input-block:focus-within {
+    border-color: #1b75d0;
+  }
+  .input-block:focus-within .input-label {
+    color: rgba(140, 117, 105, 0.8);
+  }
+
+
+  @media (max-width: 750px) {
+    .modal-container {
+      max-width: 90vw;
+    }
+
+    .modal-right {
+      display: none;
+    }
+  }
+`;
+
+export default Registration;
